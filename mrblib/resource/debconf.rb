2e7f46775be45efc45c2f45fdef58fcf85cd030a
takumin <takumiiinn@gmail.com>
2019-08-23 21:49:53 +0900

    initial implemented

--- mrblib/resource/debconf.rb
+++ mrblib/resource/debconf.rb
@@ -2,13 +2,13 @@ module ::MItamae
   module Plugin
     module Resource
       class Debconf < ::MItamae::Resource::Base
-        define_attribute :name, type: String, default_name: true
-        define_attribute :question, type: String
-        define_attribute :unseen, type: [TrueClass, FalseClass]
-        define_attribute :value, type: String
-        define_attribute :vtype, type: String
+        define_attribute :action, default: :set
+        define_attribute :package, type: String, default_name: true
+        define_attribute :question, type: String, required: true
+        define_attribute :vtype, type: String, required: true
+        define_attribute :value, type: [String, Integer, FalseClass, TrueClass, Array], required: true
 
-        self.available_actions = [:present, :absent]
+        self.available_actions = [:set, :reset]
       end
     end
   end
--- mrblib/resource_executor/debconf.rb
+++ mrblib/resource_executor/debconf.rb
@@ -2,6 +2,80 @@ module ::MItamae
   module Plugin
     module ResourceExecutor
       class Debconf < ::MItamae::ResourceExecutor::Base
+        def apply
+          if current.exists && desired.exists
+            # nothing...
+          elsif !current.exists && desired.exists
+            debconf = "#{attributes.package} #{attributes.question} #{attributes.vtype} #{attributes.value}"
+            result = run_command("echo '#{debconf}' | debconf-set-selections")
+          elsif current.exists && !desired.exists
+            # nothing...
+          elsif !current.exists && !desired.exists
+            # nothing...
+          end
+        end
+
+        private
+
+        def set_current_attributes(current, action)
+          @@debconf ||= {}
+          @@debconf[attributes.package] = {}
+
+          result = run_command(['debconf-show', attributes.package], error: false)
+          result.stdout.each_line do |raw|
+            line = raw.chomp.strip
+            case line
+            when /^(\* )?([0-9a-zA-Z-_\.\/]+): ?(.*)?$/
+              @@debconf[attributes.package][$2] = {}
+              @@debconf[attributes.package][$2][:value] = $3
+              if $1 == '* '
+                @@debconf[attributes.package][$2][:changed] = true
+              else
+                @@debconf[attributes.package][$2][:changed] = false
+              end
+            else
+              raise NotImplementedError, "[Debconf::#{attributes.package}] Regex Error"
+            end
+          end
+
+          case action
+          when :set
+            current.exists = false
+            current.package = nil
+            current.question = nil
+            current.value = nil
+
+            if @@debconf.has_key?(attributes.package)
+              current.package = attributes.package
+
+              if @@debconf[attributes.package].has_key?(attributes.question)
+                current.question = attributes.question
+
+                if @@debconf[attributes.package][attributes.question][:value] == attributes.value
+                  current.exists = true
+                  current.value = attributes.value
+                else
+                  current.value = @@debconf[attributes.package][attributes.question][:value]
+                end
+              end
+            end
+          when :reset
+            current.exists = false
+          else
+            raise NotImplementedError, "[Debconf::#{attributes.package}] Current :set or :reset"
+          end
+        end
+
+        def set_desired_attributes(desired, action)
+          case action
+          when :set
+            desired.exists = true
+          when :reset
+            desired.exists = false
+          else
+            raise NotImplementedError, "[Debconf::#{attributes.package}] Desired :set or :reset"
+          end
+        end
       end
     end
   end
